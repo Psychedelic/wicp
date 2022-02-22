8a7bc0faec721d2a75d39489786bd9dd6964318f
oz <ozwaldorf@gmail.com>
2022-02-21 22:23:21 -0500

    tooling genesis

---
+++ .scripts/cap_service.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# CAP local Service setup
+
+#!/bin/bash
+
+printf "🙏 Verifying the Cap Service status, please wait...\n\n"
+if [ -z $CAP_ID ]; then 
+    if [[ $NETWORK == "ic" ]]; then
+        CAP_ID=lj532-6iaaa-aaaah-qcc7a-cai
+    else 
+        CAP_ID=$(cd ./cap && dfx canister id ic-history-router)
+        if [ -z $CAP_ID ]; then
+            # The extra space is intentional, used for alignment
+           printf "⚠️  Warning: The Cap Service is required.\n\n"
+           read -r -p "🤖 Enter the local Cap container ID (or nothing to continue to CAP setup): " CAP_ID
+            if [ -z $CAP_ID ]; then
+                read -r -p "🤖 Would you like me to start the Cap Service for you [Y/n]? " CONT
+
+                if [ "$CONT" = "Y" ]; then
+                    npm run cap:start
+                    CAP_ID=$(cd ./cap && dfx canister id ic-history-router)
+                fi
+            fi
+        fi
+        
+    fi
+fi
+
+# the cap id should be set by now, throw an error if not
+if [ -z $CAP_ID ]; then
+    printf "Error: The CAP canister is required!\n\n"
+    exit 1
+fi
--- .scripts/healthcheck.sh
+++ .scripts/healthcheck.sh
@@ -1,1 +1,108 @@
 #/bin/bash
+# --- HEALTHCHECK.SH --- 
+
+deploy() {
+    ./.scripts/deploy.sh local
+}
+
+allowance() {
+    pem=$AlicePem
+    from="${nameToPrincipal[$1]}"
+    to="${nameToPrincipal[$2]}"
+    icx --pem=$pem query $tokenID allowance "(principal \"$from\", principal \"$to\")" $tokenPrologueXtc
+}
+
+decimals(){
+    pem=$AlicePem
+    icx --pem=$pem query $tokenID decimals "()" $tokenPrologueXtc
+}
+
+getMetadata(){
+    pem=$AlicePem
+    icx --pem=$pem query $tokenID getMetadata "()" $tokenPrologueXtc
+}
+
+historySize(){
+    pem=$AlicePem
+    icx --pem=$pem query $tokenID historySize "()" $tokenPrologueXtc
+}
+
+logo(){
+    pem=$AlicePem
+    icx --pem=$pem query $tokenID logo "()" $tokenPrologueXtc
+}
+
+name(){
+    pem=$AlicePem
+    icx --pem=$pem query $tokenID nameErc20 "()" $tokenPrologueXtc
+}
+
+symbol(){
+    pem=$AlicePem
+    icx --pem=$pem query $tokenID symbol "()" $tokenPrologueXtc
+}
+
+totalSupply(){
+    pem=$AlicePem
+    icx --pem=$pem query $tokenID totalSupply "()" $tokenPrologueXtc
+}
+
+getTransaction(){
+	txId=$1
+	pem=$AlicePem
+	icx --pem=$pem update $tokenID getTransaction "($txId)" $tokenPrologueXtc
+}
+
+getTransactions(){
+	txId=$1
+    limit=$2
+	pem=$AlicePem
+	icx --pem=$pem update $tokenID getTransactions "($txId, $limit)" $tokenPrologueXtc
+}
+
+approve(){
+	pem="${nameToPem[$1]}"
+	to="${nameToPrincipal[$2]}"
+	amount=$3
+	icx --pem=$pem update $tokenID approve "(principal \"$to\", $amount)" $tokenPrologueXtc
+}
+
+transfer(){
+	fromPem="${nameToPem[$1]}"
+	to="${nameToPrincipal[$2]}"
+	amount=$3
+	icx --pem=$fromPem update $tokenID transfer "(principal \"$to\", $amount)" $tokenPrologueXtc
+}
+
+transferFrom(){
+	from="${nameToPrincipal[$1]}"
+	to="${nameToPrincipal[$2]}"
+	amount=$3
+	callerPem="${nameToPem[$1]}"
+	if [ "$#" -eq 4 ]; then
+    	callerPem="${nameToPem[$4]}"
+	fi
+	icx --pem=$callerPem update $tokenID transferFrom "(principal \"$from\",principal \"$to\", $amount)" $tokenPrologueXtc
+}
+
+balanceOf(){
+	pem=$AlicePem
+	account="${nameToPrincipal[$1]}"
+	icx --pem=$pem query $tokenID balanceOf "(principal \"$account\")" $tokenPrologueXtc
+}
+
+tests() {
+    deploy
+    name
+    symbol
+    # logo
+    decimals
+    getMetadata
+    historySize
+    totalSupply
+
+    allowance
+}
+
+source .scripts/identity.sh # setup temporary identities
+deploy
